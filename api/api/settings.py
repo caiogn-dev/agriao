d487cc7ad37d0126a209c0d9b22df39f520c4584
Caio Gonçalves Nascimento <caiogn2002@gmail.com>
2025-06-23 20:48:44 +0000

    ajustando

--- api/api/settings.py
+++ api/api/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i!462hkjn9e43ox)u*pe=yw8qnd@v0yjv=$a5a@@=h^@d+%v0_"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
