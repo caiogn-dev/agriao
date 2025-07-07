395ada4355a68b728c4807b9453ea899b7c5f0e7
caiogn-dev <caiogn2002@gmail.com>
2025-07-07 01:53:23 -0300

    allowed hosts

--- server/server/settings.py
+++ server/server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-tlkh-ov=p#81qxthjd1whw$2l06gh5h9iv#vfwn$q=0)^%#&#s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agriao-7woe.onrender.com', 'localhost',]
 
 
 # Application definition
